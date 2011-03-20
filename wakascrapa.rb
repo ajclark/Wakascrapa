7b419aef06cfa5f1a3a8313e860509c06ed7f215
Allan Clark <napta2k@gmail.com>
2011-03-20 20:39:02 +0000

    Replaced e-mail address with github link

--- wakascrapa.rb
+++ wakascrapa.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
 # Wakascrapa v0.1 
-# by Allan Clark - <napta2k@gmail.com>
+# by Allan Clark - http://github.con/ajclark/Wakascrapa/
 # This script will download all images in a given category of a wakaba imageboard
-# URL: http://github.com/ajclark/Wakascrapa/
 
 require 'rubygems'
 require 'mechanize'
